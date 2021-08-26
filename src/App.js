4374fbe7d77723c3ddd995c688a0199f06f63664
bgraczyk <robgrac94@gmail.com>
2021-08-26 12:15:20 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navigation from "./screens/navigationBar/NavigationBar";
 import ParticleAnimation from "react-particle-animation";
+import Header from "./screens/header/Header";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Header />
       <ParticleAnimation
         style={{ height: "96vh" }}
         background={{ r: 225, g: 135, b: 155, a: 5 }}
---
+++ src/screens/header/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./header.css";
+import Typed from "react-typed";
+
+function header() {
+  return (
+    <div className="header">
+      <h1>Who am I?</h1>
+      <Typed
+        strings={[
+          " ",
+          "Software Engineer",
+          " ",
+          "Full stack developer",
+          " ",
+          "Passion for Front-end development",
+          " ",
+          "Problem Solver",
+          " ",
+          " ",
+        ]}
+        typeSpeed={70}
+        backSpeed={40}
+        loop
+      />
+    </div>
+  );
+}
+
+export default header;
